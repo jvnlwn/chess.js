45d9f4df086f2f05028485c99239cb71e8aea8c6
Joe VanLeeuwen <jvnlwn@gmail.com>
2013-10-16 14:30:46 -0400

    switched to id's. Removed unnecessary views. Moved some view functionality out of the view into main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,19 +9,57 @@
 		2: [lightSquare, darkSquare],
 	}
 
+	var top = {
+		'8': '0%',
+		'7': '12.5%',
+		'6': '25%',
+		'5': '37.5%',
+		'4': '50%',
+		'3': '62.5%',
+		'2': '75%',
+		'1': '87.5%'
+	}
+
+	var left = {
+		'a': '0%',
+		'b': '12.5%',
+		'c': '25%',
+		'd': '37.5%',
+		'e': '50%',
+		'f': '62.5%',
+		'g': '75%',
+		'h': '87.5%'
+	}
+
+
 	for (i = 0; i < 64; i++) {
 		var rankValue = rank[Math.floor(i / 8)];
 		var fileValue = file[i % 8];
 
-		if (rankValue === '2') {
-			var piece = 'pawn'
-		} else {var piece = undefined}
+		var cssPosition = {
+			top: top[rankValue],
+			left: left[fileValue]
+		}
 
-		new SquareView({
-			model: new Square({piece: piece}),
-			rank: rankValue,
-			file: fileValue,
-			color: colorCycles[rankValue % 2 + 1][i % 2]
-		})
+		var cssSquare = $.extend({
+			width: '12.5%',
+			height: '12.5%',
+			position: 'absolute',
+			background: colorCycles[rankValue % 2 + 1][i % 2]
+		}, cssPosition) 
+
+		var square = '<div class="board-square"></div>';
+		$('.container').prepend($(square).css(cssSquare))
+
+		if (rankValue === '2') {
+
+			new SquareView({
+				model: new Square({piece: 'pawn'}),
+				rank: rankValue,
+				file: fileValue,
+				cssPosition: cssPosition,
+				id: fileValue + rankValue
+			})
+		} 
 	}
 })();
--- app/scripts/views.js
+++ app/scripts/views.js
@@ -4,7 +4,7 @@ SquareView = Backbone.View.extend({
 
 	events: {
 		'mouseup': 'sayPosition',
-		'mousedown': 'bringForwrd'
+		'mousedown': 'bringToFront'
 	},
 
 	initialize: function(options) {
@@ -15,54 +15,17 @@ SquareView = Backbone.View.extend({
 	},
 
 	position: function() {
-		var top = {
-			'8': '0%',
-			'7': '12.5%',
-			'6': '25%',
-			'5': '37.5%',
-			'4': '50%',
-			'3': '62.5%',
-			'2': '75%',
-			'1': '87.5%'
-		}
+		this.$el.css(this.options.cssPosition);
+	},
 
-		var left = {
-			'a': '0%',
-			'b': '12.5%',
-			'c': '25%',
-			'd': '37.5%',
-			'e': '50%',
-			'f': '62.5%',
-			'g': '75%',
-			'h': '87.5%'
-		}
-
-		var cssPosition = {
-			top: top[this.options.rank],
-			left: left[this.options.file]
-		}
-
-		this.$el.css(cssPosition);
-
-		var cssSquare = $.extend({
-			width: '12.5%',
-			height: '12.5%',
-			position: 'absolute',
-			background: this.options.color
-		}, cssPosition) 
-
-		var square = '<div class="board-square"></div>';
-		$('.container').last().prepend($(square).css(cssSquare))
-
-		var id = this.options.file + this.options.rank
-		this.$el.attr('id', id)
-		this.$el.addClass(id)
-
-		return this.options.file + this.options.rank
+	setId: function() {
+		this.$el.attr('id', this.options.id)
+		return this.options.id;
 	},
 
 	render: function() {
-		var id = '.' + this.position();
+		this.position();
+		var id = '#' + this.setId();
 		var elem = document.querySelector(id);
 		new Draggabilly(elem);
 
@@ -75,7 +38,6 @@ SquareView = Backbone.View.extend({
 		var that = this;
 		var id = '#' + this.$el.attr('id');
 		setTimeout(function(){
-			// that.position();	
 			that.snapToSquare(id)
 		},100)
 	},
@@ -112,7 +74,7 @@ SquareView = Backbone.View.extend({
 		})
 	},
 
-	bringForwrd: function() {
+	bringToFront: function() {
 		this.$el.css('z-index', '100')
 	},
 
@@ -132,86 +94,12 @@ SquareView = Backbone.View.extend({
 	}
 })
 
-// var leftVal = parseInt(($('.a2').css('left')).slice(0, -2))
-// var topVal = parseInt(($('.a2').css('top')).slice(0, -2))
-// var closestLeftVal = parseInt($('.container').css('width').slice(0, -2))
-// var closestTopVal = parseInt($('.container').css('height').slice(0, -2));
-// var destinationLeftVal = closestLeftVal
-// var destinationTopVal = closestTopVal
-
-// $('.board-square').each(function(){
-//     var possibleLeftVal = parseInt(($(this).css('left')).slice(0, -2))
-//     var possibleTopVal = parseInt(($(this).css('top')).slice(0, -2))
-
-//     if (Math.abs(leftVal - possibleLeftVal) <= closestLeftVal) {
-//     	closestLeftVal = Math.abs(leftVal - possibleLeftVal);
-//         destinationLeftVal = possibleLeftVal
-//     }
-
-//     if (Math.abs(topVal - possibleTopVal) <= closestTopVal) {
-//     	closestTopVal = Math.abs(topVal - possibleTopVal);
-//         destinationTopVal = possibleTopVal
-//     }
-// })
-
-
-
 // function onDragMove( instance, event, pointer ) {
 //   console.log( 'dragMove on ' + event.type +
 //     pointer.pageX + ', ' + pointer.pageY +
 //     ' position at ' + instance.position.x + ', ' + instance.position.y );
 // }
 
-
-// var leftVal = parseInt(($('.a2').css('left')).slice(0, -2))
-// var topVal = parseInt(($('.a2').css('top')).slice(0, -2))
-// var closestLeftVal = parseInt($('.container').css('width').slice(0, -2))
-// var closestTopVal = parseInt($('.container').css('height').slice(0, -2));
-// var destinationLeftVal = closestLeftVal
-// var destinationTopVal = closestTopVal
-
-// $('.board-square').each(function(){
-//     var possibleLeftVal = parseInt(($(this).css('left')).slice(0, -2))
-//     var possibleTopVal = parseInt(($(this).css('top')).slice(0, -2))
-//     console.log('possibleLeft: ', possibleLeftVal)
-//     console.log('possibleTop: ', possibleTopVal)
-//     console.log('diffLeft : ', Math.abs(leftVal - possibleLeftVal))
-//     console.log('diffTop : ', Math.abs(topVal - possibleTopVal))
-
-//     if (Math.abs(leftVal - possibleLeftVal) <= closestLeftVal) {
-//     	closestLeftVal = Math.abs(leftVal - possibleLeftVal);
-//         destinationLeftVal = possibleLeftVal
-//         // console.log('yo i\'m left: ', closestLeftVal + 'because: ', Math.abs(leftVal - possibleLeftVal) <= closestLeftVal)
-//         // console.log('yo i\'m left: ',leftVal , possibleLeftVal , closestLeftVal)
-//     }
-
-//     if (Math.abs(topVal - possibleTopVal) <= closestTopVal) {
-//     	closestTopVal = Math.abs(topVal - possibleTopVal);
-//         destinationTopVal = possibleTopVal
-//         // console.log('yo i\'m top: ', closestTopVal + 'because: ', Math.abs(topVal - possibleTopVal) <= closestTopVal)
-//         // console.log('yo i\'m top: ', topVal , possibleTopVal , closestTopVal)
-//     }
-
-//     console.log('closestLeftVal: ', closestLeftVal)
-//     console.log('closestTopVal: ', closestTopVal)
-//     console.log('destinationLeftVal: ', destinationLeftVal)
-//     console.log('destinationTopVal: ', destinationTopVal)
-
-//     console.log(' ')
-// })
-
-// destinationLeftVal = destinationLeftVal.toString() + 'px';
-// destinationTopVal  = destinationTopVal.toString() + 'px';
-
-// $('.a2').css({
-// 	left: destinationLeftVal,
-// 	top: destinationTopVal
-// })
-
-
-// $('.board-square').each(function(){
-//     console.log(parseInt(($('.a2').css('left')).slice(0, -2)))
-// })
 
 
 
