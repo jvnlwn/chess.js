0c661f035163b6d0651b9edae8e80064c730ddd3
Joe VanLeeuwen <jvnlwn@gmail.com>
2013-10-21 15:07:40 -0400

    squares in between are known

--- app/scripts/views.js
+++ app/scripts/views.js
@@ -43,7 +43,8 @@ PieceView = Backbone.View.extend({
 			var newId = that.reassignId(that, newPercentages)
 			var pathDetails = that.isAPath(that, id, newId)
 			that.isPathKnown(pathDetails)
-			that.extraDependencies(pathDetails, newId)
+			var dependenciesPass = that.extraDependencies(pathDetails, newId)
+			that.generalDependencies(pathDetails, dependenciesPass, newId)
 		},100)
 
 		// if (this.model.options.token === 'K' ) {
@@ -54,8 +55,9 @@ PieceView = Backbone.View.extend({
 	isAPath: function(that, id, newId) {
 		var pathDetails = {
 			path:     false,
-			distance: 0
-			// direction
+			distance: 0,
+			fileDiff: {},
+			rankDiff: {}
 		}
 
 		var files = {
@@ -68,30 +70,21 @@ PieceView = Backbone.View.extend({
 			target:   newId.slice(1)
 		}
 
-		var fileDiff = {}
-		var rankDiff = {}
+		var fileDiff = {
+			original: that.options.fileArray.indexOf(files.original),
+			target:   that.options.fileArray.indexOf(files.target)
+		}
 
-		that.options.fileArray.forEach(function(file, index){
-			if (files.original === file) {
-				fileDiff.original = index;
-			}
-			if (files.target === file) {
-				fileDiff.target = index;
-			}
-
-		})
-
-		that.options.rankArray.forEach(function(rank, index){
-			if (ranks.original === rank) {
-				rankDiff.original = index;
-			}
-			if (ranks.target === rank) {
-				rankDiff.target = index;
-			}
-		})
+		var rankDiff = {
+			original: that.options.rankArray.indexOf(ranks.original),
+			target:   that.options.rankArray.indexOf(ranks.target)
+		}
 
 		fileDiff.diff = Math.abs(fileDiff.original - fileDiff.target)
 		rankDiff.diff = Math.abs(rankDiff.original - rankDiff.target)
+
+		pathDetails.fileDiff = fileDiff;
+		pathDetails.rankDiff = rankDiff;
 
 
 		// cascades: a file or rank path with distance of 3 will overide this
@@ -103,16 +96,36 @@ PieceView = Backbone.View.extend({
 		if (fileDiff.diff === 0) {
 			pathDetails.path = 'file';
 			pathDetails.distance = rankDiff.diff;
+			pathDetails.innerSquares = innerSquares(that.options.rankArray, rankDiff, files).squares
 		}
 
 		if (rankDiff.diff === 0) {
 			pathDetails.path = 'rank';
 			pathDetails.distance = fileDiff.diff;
+			pathDetails.innerSquares = innerSquares(that.options.fileArray, fileDiff, ranks).squares
 		}
 
 		if (Math.abs(fileDiff.diff - rankDiff.diff) === 0) {
 			pathDetails.path = 'diagonal';
 			pathDetails.distance = fileDiff.diff;
+
+			var rankSlice = innerSquares(that.options.rankArray, rankDiff, files)
+			var fileSlice = innerSquares(that.options.fileArray, fileDiff, ranks)
+
+			var combinedSlices = [];
+			var x = rankSlice.squares.length
+
+			if (rankSlice.direction === fileSlice.direction) {
+				for (i = 0; i < x; i++) {
+					combinedSlices.push(fileSlice.squares[i] + rankSlice.squares[i])
+				}
+			} else {
+				for (i = 0; i < x; i++) {
+					combinedSlices.push(fileSlice.squares[i] + rankSlice.squares[Math.abs(i - (x - 1))])
+				}		
+			}
+
+			pathDetails.innerSquares = combinedSlices;
 		}
 
 		// piece wasn't moved from square
@@ -125,6 +138,42 @@ PieceView = Backbone.View.extend({
 			pathDetails.direction = 'forward';
 		}
 
+		function innerSquares(array, change, unchanged) {
+			var startingSquare;
+			var endingSquare;
+			var inBetween = {
+				squares: [],
+				direction: 'greater'
+			};
+
+			if (pathDetails.distance > 1) {
+				if (change.original < change.target) {
+					startingSquare = change.original + 1;
+					endingSquare = change.target;
+				} else {
+					startingSquare = change.target + 1;
+					endingSquare = change.original;
+					inBetween.direction = 'lesser';
+				}		
+			}
+
+			for(i = startingSquare; i < endingSquare; i++) {
+				if (pathDetails.path === 'rank') {
+					inBetween.squares.push(array[i].toString() + unchanged.original)
+				}
+
+				if (pathDetails.path === 'file') {
+					inBetween.squares.push(unchanged.original + array[i].toString())
+				}
+
+				if (pathDetails.path === 'diagonal') {
+					inBetween.squares.push(array[i])
+				}
+			}
+
+			return inBetween
+		}
+		
 		return pathDetails;
 	},
 
@@ -144,9 +193,9 @@ PieceView = Backbone.View.extend({
 	},
 
 	extraDependencies: function(pathDetails, newId) {
+		var dependenciesPass = true;
 		if (pathDetails.path && this.model.dependencies) {
 			// path is good but check these dependencies
-			var dependenciesPass = true;
 			var dependencies = this.model.dependencies[pathDetails.path]
 
 			if (dependencies.range < pathDetails.distance) {
@@ -173,13 +222,31 @@ PieceView = Backbone.View.extend({
 				if (dependencies.occupied === this.model.options.opponent) {
 					var pieceIsThere = blackPieces.findWhere({position: newId}) || whitePieces.findWhere({position: newId}) || false;
 
-					if ((!pieceIsThere) || (pieceIsThere.player !== this.model.options.opponent)) {
+					// console.log('huh' + pieceIsThere.options.player)
+					if ((!pieceIsThere) || (pieceIsThere.options.player !== this.model.options.opponent)) {
 						dependenciesPass = false;
 					}
 				}
 			}
 
-			return dependenciesPass;
+		}
+		return dependenciesPass;
+	},
+
+	generalDependencies: function(pathDetails, dependenciesPass, newId) {
+		if (dependenciesPass) {
+			var pieceIsThere = blackPieces.findWhere({position: newId}) || whitePieces.findWhere({position: newId}) || false;
+
+			if ((pieceIsThere) && (pieceIsThere.options.player !== this.model.options.opponent)) {
+				dependenciesPass = false;
+				console.log('you already here dude')
+			} else if (pathDetails.path !== 'l-shape') {
+				if (pathDetails.distance > 1) {
+					if (pathDetails.path === 'file') {
+
+					}
+				}
+			}
 		}
 	},
 
